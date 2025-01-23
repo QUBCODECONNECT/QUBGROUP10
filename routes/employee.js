0b3069d4e0cfcdd981b68c15662cda39ac53b217
Chris Foreman <cforeman01@qub.ac.uk>
2025-01-23 14:35:46 +0000

    view employees complete

--- routes/employee.js
+++ routes/employee.js
@@ -5,25 +5,12 @@ var router = express.Router();
 const EmployeeService = require('../services/employeeService');
 const employeeService = new EmployeeService();
 
-/*
-// Create a new user form
-router.get('/add', (req, res) => {
-  res.render('addUser')
-});
-
-// Create a new user submit
-router.post('/add', (req, res) => {
-  const newUser = req.body;
-  const createdUser = userService.createUser(newUser);
-  res.redirect('/users/' + createdUser.id)
-});
-
-// Read all users
+// Read all employees
 router.get('/', (req, res) => {
-  const users = userService.getAllUsers();
-  res.render('users', { users: users })
-});
-*/
+    const employees = employeeService.getAllEmployees();
+    res.render('employees', { employees: employees })
+  });
+  
 
 // Read a employee by ID
 router.get('/:id', (req, res) => {
@@ -32,34 +19,5 @@ router.get('/:id', (req, res) => {
   res.render('employee', { employee: employee })
 });
 
-
-
-// Update a user by ID form
-router.get('/update/:id', (req, res) => {
-  const user = userService.getUserById(parseInt(req.params.id));
-  if (!user) return res.status(404).send('User not found');
-  res.render('updateUser', {user: user})
-});
-
-// Update a user by ID
-router.post('/update/:id', (req, res) => {
-  const updatedUser = userService.updateUser(parseInt(req.params.id), req.body);
-  if (!updatedUser) return res.status(404).send('User not found');
-  res.redirect('/users/' + updatedUser.id)
-});
-
-// Delete a user by ID form
-router.get('/delete/:id', (req, res) => {
-  const user = userService.getUserById(parseInt(req.params.id));
-  if (!user) return res.status(404).send('User not found');
-  res.render('deleteUser', {user: user})
-});
-
-// Delete a user by ID
-router.post('/delete/:id', (req, res) => {
-  const deletedUser = userService.deleteUser(parseInt(req.params.id));
-  if (!deletedUser) return res.status(404).send('User not found');
-  res.redirect('/users')
-});
 
 module.exports = router;
